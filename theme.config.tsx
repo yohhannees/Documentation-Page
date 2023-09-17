8d9edbaa37564fabd8e0f01217d4f6215008d8de
Yohannees <yohannesgetachewerieso@gmail.com>
2023-09-17 23:41:51 +0300

    stories, about , getting started , cntributers,

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <h4>Perago</h4>,
-
+  // logo: <img src="http://peragosystems.com/assets/images/perago2.png"></img>,
+  logo:<p>perago</p>,
   project: {
-    link: 'http://196.189.44.48/Perago/eService-training/_git/eService-training',
+    link: "http://196.189.44.48/Perago/eService-training/_git/eService-training",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Perago E-Service Documentation',
+    text: "Perago E-Service Documentation",
   },
-}
+};
 
 export default config
